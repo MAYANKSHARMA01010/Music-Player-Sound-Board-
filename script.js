619606f5dcab4cdd6ca4aa8708166d430f7768a0
MAYANKSHARMA01010 <sharmamayank01010@gmail.com>
2025-01-29 10:03:35 +0530

    Project Done

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('sound1').addEventListener('click', function() {
+    const audio = new Audio('Assets/cartoon-trombone-sound-effect-241387.mp3');
+    audio.play();
+})
+
+document.getElementById('sound2').addEventListener('click', function() {
+    const audio = new Audio('Assets/drop-sound-effect-240899.mp3');
+    audio.play();
+})
+
+document.getElementById('sound3').addEventListener('click', function() {
+    const audio = new Audio('Assets/whoosh-sound-effect-240257.mp3');
+    audio.play();
+})
